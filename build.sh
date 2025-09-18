e1a414d556e7f80d5bad141843a5483e27e63bfa
munawr <munawart4tirur@gmail.com>
2025-09-18 14:55:22 +0530

    Fix Vercel config for static deployme22

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Install Dart
-sudo apt-get update
-sudo apt-get install -y apt-transport-https
-sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /usr/share/keyrings/dart-archive-keyring.gpg'
-sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-
-sudo apt-get update
-sudo apt-get install dart
-
-# Run the Jaspr build
+# Dart is already available in Vercel's build environment
+# Just run the Jaspr build
 dart pub get
+dart pub global activate jaspr_cli
 jaspr build
